11869b9821ae1c1ed2efa06a21d17159c3932c10
Matias Barra <matias.barra@alumnos.uach.cl>
2023-07-03 19:05:13 -0400

    rm django from script

--- fix_db_permissions.py
+++ fix_db_permissions.py
@@ -1,16 +1,22 @@
-from django.conf import settings
 from metabase.views.connector.create_role_and_policy import create_user_with_policy
 
+import os
 
 if __name__ == "__main__":
     user = "valeriahenriquez"
     password = "valeriahenriquez"
+    CONSOLIDADA_DATABASE = os.getenv("CONSOLIDADA_DATABASE")
+    CONSOLIDADA_USER = os.getenv("CONSOLIDADA_USER")
+    CONSOLIDADA_IP = os.getenv("CONSOLIDADA_IP")
+    CONSOLIDADA_PASSWORD = os.getenv("CONSOLIDADA_PASSWORD")
+    CONSOLIDADA_PORT = os.getenv("CONSOLIDADA_PORT")
+    
     create_user_with_policy(
-                    dbname=settings.CONSOLIDADA_DATABASE,
-                    dbuser=settings.CONSOLIDADA_USER,
-                    dbhost=settings.CONSOLIDADA_IP,
-                    dbpassword=settings.CONSOLIDADA_PASSWORD,
-                    dbport=settings.CONSOLIDADA_PORT,
+                    dbname=CONSOLIDADA_DATABASE,
+                    dbuser=CONSOLIDADA_USER,
+                    dbhost=CONSOLIDADA_IP,
+                    dbpassword=CONSOLIDADA_PASSWORD,
+                    dbport=CONSOLIDADA_PORT,
                     username=user,
                     password=password,
                 )
