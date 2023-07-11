b7df257622c793e0975a550421b67523f2d2a71c
Matias Barra <matias.barra@alumnos.uach.cl>
2023-07-11 16:41:44 -0400

    email and pass login

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GithubLogin, GithubCallback
+from .views import GithubLogin, GithubCallback, RegularLogin
 
 urlpatterns = [
     path("login/github/", GithubLogin.as_view(), name="github_login"),
+    path("login/", RegularLogin.as_view(), name="regular_login"),
     path("github/callback/", GithubCallback.as_view(), name="github_callback")
 ]
